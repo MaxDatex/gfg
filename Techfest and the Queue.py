f430f54e9899ed0c71b243367b93e59dbafab06b
MaxDatex <60015374+MaxDatex@users.noreply.github.com>
2024-01-06 13:54:49 +0200

    Add description

--- Techfest and the Queue.py
+++ Techfest and the Queue.py
@@ -1,3 +1,16 @@
+'''
+A Techfest is underway, and each participant is given a ticket with a unique number. 
+Organizers decide to award prize points to everyone who has a ticket ID between a and b (inclusive). 
+The points given to a participant with ticket number x will be the sum of powers of the prime factors of x.
+
+For instance, if points are to be awarded to a participant with ticket number 12, 
+the amount of points given out will be equal to the sum of powers in the prime factorization of 12 (2^2 × 3^1), 
+which will be 2 + 1 = 3.
+
+Given a and b, determine the sum of all the points that will be awarded to the participants with ticket numbers between a and b (inclusive).
+'''
+
+
 from typing import List
 from collections import defaultdict
 
