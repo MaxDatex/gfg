41b26ab4d5275c012e6de59767184cc2d4f61bb3
MaxDatex <60015374+MaxDatex@users.noreply.github.com>
2024-01-06 13:50:24 +0200

    Add description

--- Largest Sum Subarray of Size at least K.py
+++ Largest Sum Subarray of Size at least K.py
@@ -1,3 +1,9 @@
+'''
+Given an array a of length n and a number k, find the largest sum of the subarray containing at least k numbers. 
+It is guaranteed that the size of array is at-least k.
+'''
+
+
 class Solution():
     def maxSumWithK(self, a, n, k):
         l, r = 0, 0
